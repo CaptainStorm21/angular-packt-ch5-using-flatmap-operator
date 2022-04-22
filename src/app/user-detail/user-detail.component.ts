f19c3f8a40c06d1f799bc84de6beeb57249a986f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-04-21 18:07:34 -0700

    step 5 - update on the behavior

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -3,7 +3,8 @@ import { Observable } from 'rxjs';
 import { UserService } from '../core/services/user.service';
 import { IUser } from '../core/interfaces/user.interface';
 import { ActivatedRoute } from '@angular/router';
-import { takeWhile} from 'rxjs/operators';
+import { takeWhile, flatMap} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-user-detail',
@@ -24,11 +25,21 @@ export class UserDetailComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.isComponentAlive = true;
     this.route.paramMap.pipe(
-      takeWhile(() => !!this.isComponentAlive)
-    ).subscribe((params) => {
-      const userId = params.get('uuid');
-      this.similarUsers = this.userService.getSimilarUsers(userId);
-      this.user = this.userService.getUser(userId);
+      takeWhile(() => !!this.isComponentAlive),
+      flatMap(params => {
+        this.user = null;
+        this.similarUsers = null;
+        const userId = params.get('uuid');
+        return this.userService.getUser(userId)
+          .pipe(
+            flatMap((user: IUser) => {
+              this.user = user;
+              return this.userService.getSimilarUsers(userId)
+            })
+          );
+      })
+    ).subscribe((similarUsers: IUser[]) => {
+      this.similarUsers = similarUsers;
     })
   }
 
